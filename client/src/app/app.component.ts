90542ae3e1bc49d51ff7d94fa97d8763cc2b54f7
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-21 14:43:51 +0530

    update 'app' compoent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,HomeComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+  
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", component:HomeComponent
+    },
+    {
+        path:"aboutus",
+        component:AboutComponent
+    },
+    
+    {
+        path:"contactus",
+        component:ContactComponent
+    }
+];
