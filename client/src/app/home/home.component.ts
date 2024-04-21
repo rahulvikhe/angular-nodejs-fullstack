96fa8a220f126b52b8abdb4487bdfb2edd576642
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-21 14:44:35 +0530

    update 'home' component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchdataService } from '../fetchdata.service';
+import { ReturnStatement } from '@angular/compiler';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private x1:FetchdataService){}
 
+  public record:any;
+
+ ngOnInit() {
+   console.log('fetch node js data');
+   
+   this.x1.apiFromnodejs().subscribe((result)=>{
+    console.log(result);
+    this.record = result;
+   });
+ }
 }
