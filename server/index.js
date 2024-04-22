517b78f0810b7c4077acd5dd67f70cb36c10c768
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-22 11:51:57 +0530

    Update db name

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '', 
-  database: 'angular-node-db'
+  database: 'angular-nodejs-db'
 });
 
 // Creating an Express application
